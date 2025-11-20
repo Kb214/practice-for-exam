542908fd9338a27d0eae88417c7add7d3307cf8b
Karleigh Branscomb <kbranscomb@ualr.edu>
2025-11-20 23:19:42 +0000

    simple sequential loop program with no storage for games

--- main.cpp
+++ main.cpp
@@ -9,7 +9,20 @@
         genre 
         year
         rating out of 5 stars
+    STORE:
+        into a class
+        store that class into a vector
+        make functions
+
 */
+
+class Game{
+
+};
+
+class GameList{
+
+};
 
 int main(){
     int choice{0};
@@ -17,7 +30,6 @@ int main(){
     float rating;
     std::string name;
     std::string genre;
-    
     
     
     std::cout << "\n======== Gaming List ========\n";
@@ -34,45 +46,61 @@ int main(){
     std::cin >> choice;
     std::cin.ignore();
     
-    if (choice == 1){
+    while(choice != 4){
+
+        if (choice == 1){
         
-        std::cout << "======== Enter Your Game's Details: ========\n";
-        std::cout << "Name: ";
-        std::getline(std::cin, name);
+            std::cout << "\n======== Enter Your Game's Details: ========\n";
+            std::cout << "Name: ";
+            std::getline(std::cin, name);
+            
+            std::cout << "Year: ";
+            std::cin >> year;
+            std::cin.ignore();
+            
+            std::cout << "Genre: ";
+            std::getline(std::cin, genre);
+            
+            std::cout << "Rating(Out of 5): ";
+            std::cin >> rating;
+            
+            
+            std::cout << "\n======== Your Added Game ========\n";
+            std::cout << "Game: " << name << "\n" << "Year: " << year << "\n" << "Genre: " << genre << "\n" << "Rating: " << rating << "\n";
+            
+
+            std::cout << "\n======== Would you like to:========\n";
+            std::cout << "1) ADD Game\n";
+            std::cout << "2) REMOVE Game\n";
+            std::cout << "3) CHANGE Rating\n";
+            std::cout << "4) Exit\n";
+
+
+        }else if (choice == 2){
+            std::cout << "\n======== Enter your game you want to remove: ========\n";
+            
+            std::cout << "Remove Game:";
+            std::getline(std::cin, name); 
+            
+            std::cout << "\n======== Remove Game! ========\n";
         
-        std::cout << "Year: ";
-        std::cin >> year;
+        }else if (choice == 3 ){
+            std::cout << "What game rating do you want to choose? \n";
+            
+            std::cout << "Game Name: ";
+            std::getline(std::cin, name);
+            
+            std::cout << "Change your rating: ";
+            std::cin >> rating; 
+            
+        }else{
+            choice == 4; 
+        }
+
+        std::cout << "\nChoice: ";
+        std::cin >> choice;
         std::cin.ignore();
-        
-        std::cout << "Genre: ";
-        std::getline(std::cin, genre);
-        
-        std::cout << "Rating(Out of 5): ";
-        std::cin >> rating;
-        
-        
-        std::cout << "======== Your Added Game ========\n";
-        std::cout << "Game: " << name << "\n" << "Year: " << year << "\n" << "Genre: " << genre << "\n" << "Rating: " << rating << "\n";
-        
-    }else if (choice == 2){
-        std::cout << "======== Enter your game you want to remove: ========\n";
-        
-        std::cout << "Remove Game:";
-        std::getline(std::cin, name); 
-        
-        std::cout << "\n======== Remove Game! ========\n";
-        
-    }else if (choice ==3 ){
-        std::cout << "What game rating do you want to choose? \n";
-        
-        std::cout << "Game Name: ";
-        std::getline(std::cin, name);
-        
-        std::cout << "Change your rating: ";
-        std::cin >> rating; 
-        
-    }else{
-        std::cout << "Bye bye!\n";
+    
     }
     
     
