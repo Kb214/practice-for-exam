5451ca6fff58c8eb2ab25d13aebdecd17a98a30f
Karleigh Branscomb <kbranscomb@ualr.edu>
2025-12-04 20:38:47 +0000

    file orgnization; using templates; using file input and output

---
+++ templates/simpleTemplates.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+/*allows you to make functions and classes*/
+template <typename T>
+
+T max(T a, T b){
+    /*return if a is greater than b. a if true, b if false*/
+    return (a > b) ? a : b;
+}
+
+/*need to define your template after each different function you make
+ template DO NOT carry over to other functions */
+template <typename T>  
+
+T add(T a, T b){
+    return a + b; 
+}
+/* Templates are NOT types, they are patterns*/
+
+int main(){
+    std::cout << max(4,9) << "\n";
+    std::cout << max(5,3) << "\n";
+
+    std::cout << add(4,9) << "\n";
+    std::cout << add(5,3) << "\n";
+
+
+    return 0;
+}
